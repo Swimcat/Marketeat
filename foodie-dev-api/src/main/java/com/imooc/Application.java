06225d4d9036b35d5b2ea4bdb58beb69eb287946
Swimcat <37012590+Swimcat@users.noreply.github.com>
2021-03-22 10:11:59 +0800

    Revert "feat(init): ready eat spring boot"

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,9 +2,8 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages="controller")
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- foodie-dev-api/src/main/java/controller/HelloController.java
+++ foodie-dev-api/src/main/java/controller/HelloController.java
@@ -1,9 +1,7 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class HelloController {
     @GetMapping("/hello")
     public Object hello(){
